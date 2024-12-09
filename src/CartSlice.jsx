eb08d03ce27a9f8da0cf6d1a4423c9d0efdff702
cristian <cristian.triboi@gmail.com>
2024-12-09 09:20:32 -0500

    Add some logic

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -27,6 +27,9 @@ export const CartSlice = createSlice({
         },
     },
 });
+export const selectTotalQuantity = (state) => {
+    return state.cart.items.reduce((total, item) => total + item.quantity, 0);
+};
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
